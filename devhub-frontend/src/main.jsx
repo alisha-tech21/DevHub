94ff6b24e60814569791a871ca5371c360535e41
Alisha Asmat <alisha.itseries@gmail.com>
2026-06-22 21:22:42 +0500

    remove logo code main.jsx

--- devhub-frontend/src/main.jsx
+++ devhub-frontend/src/main.jsx
@@ -4,12 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./context/AuthContext.jsx"; // 1. AuthProvider import karein
-import devLogo from "./assets/devlogo.png";
 
-const link = document.querySelector("link[rel~='icon']");
-if (link) {
-  link.href = devLogo;
-}
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
